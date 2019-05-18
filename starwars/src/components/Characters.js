a4270b610fa64f1c954027b4992d1a3bf6bc7677
Myke47 <mychel.larson@gmail.com>
2019-05-17 19:11:30 -0700

    had some errors for character component, this is cleared

---
+++ starwars/src/components/Characters.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Characters = props => {
+    console.log(props.starwarsChars);
+
+    return (
+        <div className='char-card'>
+            
+        </div>
+
+    )
+
+}
+
+export default Characters;

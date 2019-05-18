45339e39061e06da11f3335cd7cef0b67aeb691b
Myke47 <mychel.larson@gmail.com>
2019-05-18 01:03:44 -0700

    style updates

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -9,6 +9,7 @@ const Characters = props => {
             <h2>{props.characters.name}</h2>
             <h3>mass:{props.characters.mass}</h3>
             <h3>dob:{props.characters.birth_year}</h3>
+            <h3>height:{props.characters.height}</h3>
 
         </div>
 
